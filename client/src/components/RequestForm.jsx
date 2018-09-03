1725642a1dd8b289f8ec10d044c33e1f14388d67
Pooja <pooja.subramanian3@gmail.com>
2018-09-03 15:40:22 -0700

    form with semantic UI

--- client/src/components/RequestForm.jsx
+++ client/src/components/RequestForm.jsx
@@ -1,66 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import { Form , TextArea, Button } from 'semantic-ui-react'
 
 class RequestForm extends React.Component{
 
-	constructor(props){
+  constructor(props){
+    super(props);
 
-	super(props);
-    
     this.state = {
       
       Subject: '',
       Message:''
     };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-	}
+    this.handleChange = this.handleChange.bind(this)
+  }
 
-  handleChange(event) {
-  	
-  	if(event.target.name==='Message'){
-    	this.setState({Message: event.target.value})
+    handleChange(event) {
+    
+    if(event.target.name==='Message'){
+      this.setState({Message: event.target.value})
     }
     else{
-    	this.setState({Subject: event.target.value})
+      this.setState({Subject: event.target.value})
     }
   }
 
-  handleSubmit(event) {
-  	console.log(event);
-    event.preventDefault();
+  render(){
+    return(
+  
+  <form action="/newreq" method="post">
+    <Form.Field>
+      <label>Subject</label>
+      <br/>
+      <input type="text" name="Subject" placeholder='Subject' value={this.state.Subject} onChange={this.handleChange} />
+    </Form.Field>
+    <Form.Field>
+      <label>Request Message : </label>
+      <br/>
+      <textarea placeholder='Tell us more ...' name="Message" value={this.state.Message} onChange={this.handleChange} />
+    </Form.Field>
+    <button type='submit'>Submit</button>
+  </form>
+    )
   }
-
-	render(){
-
-		return(
-
-			<div>
-
-			<form action="/newreq" method="post">
-  			
-  			<label>
-    		Subject:
-    		<input type="text" name="Subject" value={this.state.Subject} onChange={this.handleChange} />
-  			</label>
-  			
-  			<hr/>
-
-  			<label>
-  			Message:
-           <textarea name="Message" value={this.state.message} onChange={this.handleChange} />
-  			</label>
-
-  			<button type="submit">Submit</button>
-
-			</form>
-			
-			</div>
-
-			)
-	}
 }
 
 
 export default RequestForm;
+
+
+
+
