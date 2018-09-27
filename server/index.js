fc894ad916b313e0db8db730d5810612c2ab8c5b
jongregtom <jonthomas83@gmail.com>
2018-09-27 13:24:28 -0700

    added endpoint for homepage to add queries

--- server/index.js
+++ server/index.js
@@ -9,4 +9,9 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use('/', express.static('dist'));
 app.use('/',express.static('public'));
 
+app.get('/', function(req, res) {
+  console.log('why is this route not doing anything?')
+  res.end();
+})
+
 app.listen(3000, () => console.log('Example app listening on port 3000!')) 
